1c78bb7c320f0c505796abf746dc637c608518f5
gabriel andrews <gabem_29@yahoo.com>
2022-04-18 22:54:41 -0400

    end. i forgot to commit lol

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,22 @@ CREATE TABLE department (
   id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE job (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES job (id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -6,9 +6,19 @@ INSERT INTO department (name)
         ("paint");
 
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO job (title, salary, department_id)
     VALUES 
         ("service desk", 25000.00, 1),
         ("paint specalist", 25000.00, 2),
         ("floor manager", 75000.00, 1),
         ("paint boss", 100000.00, 2);
+
+        INSERT INTO employee (first_name, last_name, job_id, manager_id) 
+    VALUES
+        ("harry", "potter", 3, NULL),
+        ("ron", "weasly", 4, NULL);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+    VALUES
+        ("draco", "malfoy", 1, 1),
+        ("hermonie", "granger", 2, 2);
